6cb0d9c3bb20c666ca0ac09f57e5996849d95e42
Joana dos Santos Raposo <74430816+JrOkean@users.noreply.github.com>
2023-10-01 02:13:34 -0300

    atualização

---
+++ Projeto-BD/resources/views/Usuario/index.blade.php
@@ -0,0 +1,20 @@
+@extends('templates.template')
+
+@section('content')
+<h1 class="text-center">Usuários</h1><hr>
+    <div class="col-8 m-auto">
+            <table class="table">
+        <thead>
+            <tr>
+            <th scope="col">ID</th>
+            <th scope="col">Nome de Usuário</th>
+            <th scope="col">Primeiro nome</th>
+            <th scope="col">Segundo nome</th>
+            </tr>
+        </thead>
+        <tbody>
+            
+        </tbody>
+        </table>
+    </div>
+@endsection
---
+++ Projeto-BD/resources/views/templates/template.blade.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lan="pt-br">
+<head> 
+    <meta charset="UTF-8">
+    <name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>CRUD</title>
+</head>
+<body>
+    @yield('content')
+</body>    
+</html>
